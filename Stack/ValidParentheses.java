09f7d21555534a54a02c34b7fc32e65055b07a01
nikitachouhan <nikitachouhan.edu@gmail.com>
2021-07-13 14:57:12 +0530

    Create ValidParentheses.java
    20. Valid Parentheses Solution

---
+++ Stack/ValidParentheses.java
@@ -0,0 +1,27 @@
+class ValidParentheses {
+    
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<Character>();
+                
+        for(int i=0; i<s.length(); i++) {
+            char current = s.charAt(i);
+            try {
+                if(current =='(' || current == '[' || current == '{') {
+                    stack.push(current);
+                }
+                else if(( current == ')' && stack.pop()!='(')
+                        || (current == ']' && stack.pop()!='[')
+                        || (current == '}' && stack.pop()!='{')) {
+                    return false;
+                }
+            }   
+            catch(Exception e){
+                return false;    
+            }
+        }
+        if(stack.isEmpty())
+            return true;
+        else
+            return false;
+    }
+}
