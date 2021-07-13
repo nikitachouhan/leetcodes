2192d1686ba680cd73edaf291b88b52996ed6c91
nikitachouhan <nikitachouhan.edu@gmail.com>
2021-07-13 21:27:49 +0530

    Update ValidParentheses.java

--- Stack/ValidParentheses.java
+++ Stack/ValidParentheses.java
@@ -4,28 +4,24 @@
 class ValidParentheses {
     
     public boolean isValid(String s) {
+
         // Using ArrayDeque is faster than using Stack class.
         Deque<Character> stack = new ArrayDeque<Character>();
                 
         for(int i=0; i<s.length(); i++) {
             char current = s.charAt(i);
-            try {
-                if(current =='(' || current == '[' || current == '{') {
-                    stack.push(current);
-                }
-                else if(( current == ')' && stack.pop()!='(')
-                        || (current == ']' && stack.pop()!='[')
-                        || (current == '}' && stack.pop()!='{')) {
-                    return false;
-                }
-            }   
-            catch(Exception e){
-                return false;    
+            if(current =='(' || current == '[' || current == '{') {
+                stack.push(current);
+            }
+            if(stack.isEmpty()) {
+                return false;
+            }
+            else if(( current == ')' && stack.pop()!='(')
+                    || (current == ']' && stack.pop()!='[')
+                    || (current == '}' && stack.pop()!='{')) {
+                return false;
             }
         }
-        if(stack.isEmpty())
-            return true;
-        else
-            return false;
+        return (stack.isEmpty());
     }
 }
